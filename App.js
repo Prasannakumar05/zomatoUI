0cbf02db9fd1eb4cc39764081630a8993b9fff05
Prasanna Kumar <mrgprasannakumarbtechit@gmail.com>
2020-08-05 14:37:43 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import colors from "./constants/colors";
+import SearchBar from "./components/SearchBar";
+import FoodItems from "./components/FoodItems";
 
-export default function App() {
+import AppNavigator from "./navigation/AppNavigator";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <AppNavigator />
+    // {/* <ScrollView style={{ backgroundColor: colors.white }}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    //  <StatusBar style="auto" />
+    // </ScrollView> */}
+  );
+};
+export default App;
